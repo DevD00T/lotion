ea374ee21280b68ecf8780eb29c938a111a2e6ef
DevD00T <ddebargha8@gmail.com>
2023-12-23 15:36:51 +0530

    first commit

--- app/(marketing)/_components/footer.tsx
+++ app/(marketing)/_components/footer.tsx
@@ -1,19 +1,24 @@
 import { Button } from "@/components/ui/button"
-
+import Link from 'next/link';
 import { Logo } from "./logo"
+
 
 export const Footer = () => {
   return (
     <div className="flex items-center w-full p-6 bg-background z-50 dark:bg-[#1F1F1F]">
       <Logo />
       <div className="md:ml-auto w-full justify-between md:justify-end flex items-center gap-x-2 text-muted-foreground">
-        <Button variant="ghost" size="sm">
-          Privacy Policy
+        <Button variant="ghost" size="sm" asChild>
+          <Link href="/privacy-policy">
+            Privacy Policy
+          </Link>
         </Button>
-        <Button variant="ghost" size="sm">
-          Terms & Conditions
+        <Button variant="ghost" size="sm" asChild>
+          <Link href="/terms-and-conditions">
+            Terms & Conditions
+          </Link>
         </Button>
       </div>
     </div>
-  )
-}
+  );
+};
